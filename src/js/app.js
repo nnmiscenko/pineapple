10713dd3e7cc2cdbda182323655f7f2c6fa0bc68
Nikolay Miscenko <nnmiscenko@gmail.com>
2020-11-30 22:55:16 +0200

    TASK-2: Vue.js form validation implementation

---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+new Vue({
+    el: '.newsletter',
+    data() {
+      return {
+        email: '',
+        terms: false,
+        emailMessage: '',
+        termsMessage: '',
+        haveErrors: false,
+        submitted: false, 
+      }
+    },
+
+    computed: {
+      isInvalidForm() {
+        this.emailMessage = '';
+        this.termsMessage = '',
+        this.haveErrors = false;
+
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const colombiaRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[Cc][Oo]))$/;
+
+        if (this.email.length > 0) {
+          if (!emailRegex.test(this.email)) {
+            this.emailMessage = "* Please provide a valid e-mail address";
+            this.haveErrors = true
+          }
+          if (colombiaRegex.test(this.email)) {
+            this.emailMessage = "* We are not accepting subscriptions from Colombia emails";
+            this.haveErrors = true
+          }
+        } else if (this.email.length === 0) {
+          this.emailMessage = "* Email address is required";
+          this.haveErrors = true
+        }
+
+        if (this.terms === false) {
+          this.termsMessage = "* You must accept the terms and conditions";
+          this.haveErrors = true
+        }
+
+        return this.haveErrors;
+      },
+    },
+    methods: {
+      submit() {
+        if (this.isInvalidForm) return;
+        this.submitted = true;
+      }
+    }
+  })
